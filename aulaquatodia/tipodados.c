457cd93b38f23f2880e094dbcdc8e2d934c8efa6
omelhordetodos <ilo21danielzem@gmail.com>
2025-01-21 23:52:57 +0000

    carlos,

---
+++ aulaquatodia/tipodados.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main () {
+    short int numeropequeno = 32767;
+
+    printf ("Numero pequeno: %d\n", numeropequeno);
+    numeropequeno = 32768;
+
+    printf ("Numero pequeno: %d\n", numeropequeno);
+    
+    return 0;
+}
